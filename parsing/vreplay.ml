df8ce62ff744f3608ab3afe327b56daea8995960
Robyn <wuad391@gmail.com>
2026-07-21 21:34:04 +0000

    documentation

--- parsing/vreplay.ml
+++ parsing/vreplay.ml
@@ -3,8 +3,8 @@ open Parsetree
 type t = unit
 
 
-(** making a print statement for a given file *)
-(* let print_function input =
+(** This is the incomplete print node that will be completed + added in soon. *)
+(* let print_node input =
   let module_longident = Longident.Lident "Printf" in
   let print_longident = Longident.Ldot (Location.mknoloc module_longident, Location.mknoloc "printf") in 
   let print_function = Ast_helper.Exp.ident (Location.mknoloc print_longident) in
@@ -14,30 +14,35 @@ type t = unit
   Ast_helper.Exp.apply print_function [ (Nolabel, print_arg) ] *)
 
 
-let print_then_run (exp : Parsetree.expression) = 
+let print_then_run_node (exp : Parsetree.expression) = 
   Parsetree.Pexp_let (Asttypes.Nonrecursive, 
   [{pvb_pat={
     ppat_desc=Parsetree.Ppat_construct (Location.mknoloc (Longident.Lident "()"), None);
     ppat_loc=exp.pexp_loc;
     ppat_loc_stack=exp.pexp_loc_stack;
     ppat_attributes=exp.pexp_attributes;};
-  pvb_expr=exp; (* change this to print_node!! *)
+  pvb_expr=exp; (* change this to print_node when it gets implemented!! *)
   pvb_constraint=None;
   pvb_attributes=[]; 
-  pvb_loc=(exp.pexp_loc)}]
+  pvb_loc=(exp.pexp_loc)}] (* This is a pattern for let () = ... *)
     
   , exp)
-(* fix attributes!! *)
+(* i need to fix pvb_attributes, shouldn't be a [] i think. !! *)
+
+
+(* This function injects a print node onto every Pexp_apply *)
 let inject_expression _mapper (exp : Parsetree.expression) =
   match exp.pexp_desc with 
   | Pexp_apply (exp, _args) -> 
-    ({pexp_desc=print_then_run exp;
+    ({pexp_desc=print_then_run_node exp;
      pexp_loc=exp.pexp_loc;
    pexp_loc_stack=[exp.pexp_loc]; 
    pexp_attributes=exp.pexp_attributes} : Parsetree.expression)
   | _ -> exp
 
+  (* This is our special mapper that changes pexp_applies *)
 let inject_mapper = { Ast_mapper.default_mapper with 
   Ast_mapper.expr = inject_expression}
 
+  (* exposed for compile_commmon *)
 let inject_instrumentation ast = inject_mapper.Ast_mapper.structure inject_mapper ast 
